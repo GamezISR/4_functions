80cde90a11b0552ab8e2aeba1cc614ad78b13c7a
Gonzalo Gamez <88616733+GamezISR@users.noreply.github.com>
2021-11-09 12:27:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+#warm up
+'''
+Take two intergers as inputs num1 and num2
+
+if one number is greater than 10 print out the two numbers added
+if one number is less than 3 print out the two numbers subtracted 
+if neither of these conditions print them out multiplied 
+'''
+'''
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a second number: "))
+if num1 > 10 or num2 > 10:
+  print(num1 + num2)
+elif num1 < 3 or num2 < 3:
+  print(num1 - num2)
+else:
+  print(num1 * num2)
+
+'''
+
+#functions
+#defining 
+#a function with no parameters
+
+def add_two_numbers():
+  num1 = int(input("Enter a number: "))
+  num2 = int(input("Enter a second number: "))
+  print(num1 + num2)
+
+
+#calling
+add_two_numbers()
+add_two_numbers()
+add_two_numbers()
+add_two_numbers()
+
+
+
+#make a function that substracts two numbers
+#make a fucntion that multiplies three numbers
+#call each twice
+
+
+
+
